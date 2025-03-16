b6002c7948e1ea38f7a17d3b7bd5e5f798493451
LaiAnTan <tanlaian2004@gmail.com>
2025-03-16 15:17:13 +1100

    fix: cors

--- app.js
+++ app.js
@@ -40,7 +40,10 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+	origin: "http://localhost:3000",
+	credentials: true
+}));
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cookieParser());
