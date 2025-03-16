8a6a069a6f2b9195f22dcf8f7e1e996cc09bc04f
CheeMinHao <cheemh3456@gmail.com>
2025-03-16 15:52:54 +1100

    here u go

--- config/passport.js
+++ config/passport.js
@@ -11,18 +11,21 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.CALLBACK_URL,
+            callbackURL: "/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
                 let user = await User.findOne({ googleId: profile.id });
 
                 if (!user) {
-                    user = await User.create({
+                    user = new User({
                         googleId: profile.id,
-                        name: profile.displayName,
-                        email: profile.emails[0].value,
-                        avatar: profile.photos[0].value,
+                        username: profile.displayName, // Using Google profile name as username
+                        profilePicture: profile.photos[0].value,
+                        exp: 0, // Default experience points
+                        rewardsEarned: [], // Empty rewards array
+                        preferences: [], // Empty preferences array (to be set by user later)
+                        tasks: [] // Empty tasks array
                     });
                 }
 
