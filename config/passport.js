5d3ad337c599d14bdc1cccb9cd7773da1224f116
CheeMinHao <cheemh3456@gmail.com>
2025-03-16 16:56:29 +1100

    recommendation

--- config/passport.js
+++ config/passport.js
@@ -15,24 +15,6 @@ passport.use(
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
-                let user = await User.findOne({ googleId: profile.id });
-
-                if (!user) {
-                    user = new User({
-                        googleId: profile.id,
-                        username: profile.displayName, // Using Google profile name as username
-                        profilePicture: profile.photos[0].value,
-                        exp: 0, // Default experience points
-                        rewardsEarned: [], // Empty rewards array
-                        preferences: [], // Empty preferences array (to be set by user later)
-                        tasks: [] // Empty tasks array
-                    });
-
-                    user.redirectTo = "/register"; // Custom field for redirecting
-                } else {
-                    user.redirectTo = "/dashboard"; // Custom field for redirecting existing users
-                }
-
                 return done(null, user);
             } catch (error) {
                 return done(error, null);
